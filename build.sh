139cf85d4dbad5bddeef6db924735a3e21fa9bf7
Jim Schaad <ietf@augustcellars.com>
2017-05-12 13:16:37 -0700

    Change the build script for travis.

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ msbuild /v:q /p:Configuration=$VERSION $SLN
 mono ../testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./COSE.Tests/bin/$VERSION/$TARGET/COSE.Tests.dll
 
 cd ..
-cd examples
+#cd examples
 
-msbuild /v:q /p:Configuration=$VERSION examples.sln
+#msbuild /v:q /p:Configuration=$VERSION examples.sln
 
-mono examples/examples/bin/$VERSION/$TARGET/examples.exe --cose Regressions
-mono examples/examples/bin/$VERSION/$TARGET/examples.exe --jose cookbook
+#mono examples/examples/bin/$VERSION/$TARGET/examples.exe --cose Regressions
+#mono examples/examples/bin/$VERSION/$TARGET/examples.exe --jose cookbook
