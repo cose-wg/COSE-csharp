696e7a68713014126a026427cb71dc7d952ebd04
Jim Schaad <ietf@augustcellars.com>
2017-08-22 19:47:51 -0700

    looks like the new mono is a problem?

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ ls
 
 mono ../nuget.exe restore $SLN
 
-msbuild /p:Configuration=$VERSION $SLN
+#msbuild /p:Configuration=$VERSION $SLN
+xbuild /p:Configuration=$VERSION $SLN
 
 mono ../testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe ./COSE.Tests/bin/$VERSION/$TARGET/COSE.Tests.dll
 
